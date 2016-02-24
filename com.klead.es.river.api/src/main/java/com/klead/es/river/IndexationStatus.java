9f67b991b074bf1477c6186bec4de4349f7f6519
khaledAFI <shinigami501>
2016-02-24 12:13:39 +0100

    add transactional ascpect to handel exceptions in marking non indexed data

---
+++ com.klead.es.river.api/src/main/java/com/klead/es/river/IndexationStatus.java
@@ -0,0 +1,7 @@
+package com.klead.es.river;
+
+/**
+ * Created by kafi on 24/02/2016.
+ */
+public enum IndexationStatus {
+}
--- com.klead.es.river.impl/src/main/java/com/klead/es/river/Settings.java
+++ com.klead.es.river.impl/src/main/java/com/klead/es/river/Settings.java
@@ -4,11 +4,8 @@ package com.klead.es.river;
  * Created by kafi on 12/02/2016.
  */
 public enum Settings {
-            BULK_BLOC_SIZE,
-            INDEX_NAME,
             INDEX_TYPE ,
             SHARD_NUMBER,
             REPLICA_NUMBER ,
-            PURGE_INDEX,
     INDEX_TYPE_MAPPING;
 }
